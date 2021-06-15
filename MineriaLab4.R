9d4706cb3a522ede0fb338266798ed985a401292
franco tapia <franco.tapia.c@usach.cl>
2021-06-15 17:07:07 -0400

    Algunos cambios

--- MineriaLab4.R
+++ MineriaLab4.R
@@ -18,7 +18,7 @@ graficar1 <- function(res){
 }
 
 graficar2 <- function(res){
-        viewer(resMMHC,
+        viewer(res,
                bayesianNetwork.width = "100%",
                bayesianNetwork.height = "80vh",
                bayesianNetwork.layout = "layout_on_grid",
@@ -41,6 +41,15 @@ graficar2 <- function(res){
 ########################### 1. Lectura de Datos ###########################
 data <- data.frame(alarm)
 
+modelstring = paste0("[HIST|LVF][CVP|LVV][PCWP|LVV][HYP][LVV|HYP:LVF][LVF]",
+                     "[STKV|HYP:LVF][ERLO][HRBP|ERLO:HR][HREK|ERCA:HR][ERCA][HRSA|ERCA:HR][ANES]",
+                     "[APL][TPR|APL][ECO2|ACO2:VLNG][KINK][MINV|INT:VLNG][FIO2][PVS|FIO2:VALV]",
+                     "[SAO2|PVS:SHNT][PAP|PMB][PMB][SHNT|INT:PMB][INT][PRSS|INT:KINK:VTUB][DISC]",
+                     "[MVS][VMCH|MVS][VTUB|DISC:VMCH][VLNG|INT:KINK:VTUB][VALV|INT:VLNG]",
+                     "[ACO2|VALV][CCHL|ACO2:ANES:SAO2:TPR][HR|CCHL][CO|HR:STKV][BP|CO:TPR]")
+
+dag = model2network(modelstring)
+
 ########################### 2. Preprocesamiento ###########################
 
 
@@ -55,55 +64,67 @@ str(data)
 #Datos son factores de 2,3 y 4 niveles según la variable
 
 
+
 #Lista negra
-bl<-data.frame("M..Work","Family") #Lista negra de relaciones (Origen, Destino)
-res <- hc(bn_df,blacklist = bl)
-plot(res)
 
 
-bn_df <- data
+bl <- matrix(c("HREK","HRBP","PCWP","LVV","PRSS","VTUB","ERLO","HR","HRBP","HR","KINK","DISC","INT","KINK","INT","DISC",
+               "PRSS","KINK","MINV","VALV","MINV","INT","MINV","VLNG","MINV","VTUB"),ncol=2,byrow=TRUE)
+
+wl <- matrix(c("CCHL","HR","CVP","CO","PAP","PMB"),ncol=2,byrow=TRUE)
 
 
 ########################### 3. Aplicar Método ###########################
 
-resHC <- hc(bn_df) # Algoritmo Hill-Climbing
-resMMHC <- mmhc(bn_df)  # Algoritmo MaxMin Hill-Climbing
-resMMPC <- mmpc(bn_df) # Algoritmo MaxMin Parents and Children 
+bn_df <- data
 
-#Mostrar resp.
+# Algoritmo Hill-Climbing
+resHC <- hc(bn_df,blacklist = bl, whitelist = wl)      
 print(resHC)
-print(resMMHC)
-print(resMMPC)
-
-
-### Graficar ###
+#Graficar
 graficar1(resHC)
-graficar1(resMMHC)
-graficar1(resMMPC)
-
 graficar2(resHC)
-graficar2(resMMHC)
-graficar2(resMMPC)
-
-
 #Obtener BIC
-scHC <-score(resHC,bn_df )              # BIC por default
-scMMHC <- score(resMMHC,bn_df)
-scMMPC <- score(resMMPC,bn_df)
-
+scHC <-score(resHC,bn_df )  
 print(scHC)
-print(scMMHC)
-print(scMMPC)
-
-#Propagación de la evidencia
+#Propagación de la evidencia: EM (Ejecuación de Máxima Exprectación)
 fittedbn.HC <- bn.fit(resHC, data = bn_df)
+
+# Algoritmo MaxMin Hill-Climbing
+resMMHC <- mmhc(bn_df,blacklist = bl, whitelist = wl)  
+print(resMMHC)
+#Graficar
+graficar1(resMMHC)
+graficar2(resMMHC)
+#Obtener BIC
+scMMHC <- score(resMMHC,bn_df)
+print(scMMHC)
+#Propagación de la evidencia: EM (Ejecuación de Máxima Exprectación)
 fittedbn.MMHC <- bn.fit(resMMHC, data = bn_df)
+
+# Algoritmo MaxMin Parents and Children
+resMMPC <- mmpc(bn_df,blacklist = bl, whitelist = wl)  
+print(resMMPC)
+#Graficar
+graficar1(resMMHC)
+graficar2(resMMPC)
+#Obtener BIC
+scMMPC <- score(resMMPC,bn_df)
+print(scMMPC)
+#Propagación de la evidencia: EM (Ejecuación de Máxima Exprectación)
 fittedbn.MMPC <- bn.fit(resMMPC, data = bn_df)
+
+
 
 print(fittedbn.HC$E)
 print(fittedbn.MMHC$E)
 print(fittedbn.MMPC$E)
 
+par(mfrow = c(1, 2))
+graphviz.compare(dag, resHC, layout = "fdp" ,shape = "ellipse", main = c("DAG original", "DAG propio"))
+graphviz.compare(fittedbn.HC, resHC, layout = "fdp" ,shape = "ellipse", main = c("DAG fitted", "DAG propio"))
+
+#Obtención de tabla de probabilidades condicionales mediante EM
 
 
 fittedbn <- bn.fit(res, data = bn_df) # Se obtiene la tabla de probabilidades condicionales mediante EM. (Máxima Expectación, propagación de la evidencia)
